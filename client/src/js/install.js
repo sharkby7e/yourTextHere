804f06c95934689bbe9d9e71aed0749253cd3959
sharkbyte <sgquin@gmail.com>
2022-05-26 17:35:39 -0700

    event listeners for install work, db is written to and gotten from

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,19 @@
-const butInstall = document.getElementById('buttonInstall');
+const butInstall = document.getElementById("buttonInstall");
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  butInstall.style.visibility = "visible";
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener("click", () => {
+  butInstall.setAttribute("disabled", true);
+  butInstall.textContent = "Installed!";
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener("appinstalled", (e) => {
+  console.log("app installed", e);
+});
